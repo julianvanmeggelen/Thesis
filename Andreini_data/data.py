9a66d324d6fbc61d7c42057f6206588dc8c13670
Julian van Meggelen <julianvanmeggelen@gmail.com>
2023-04-08 01:27:11 +0200

    mcmc prototype

--- Andreini_data/data.py
+++ Andreini_data/data.py
@@ -84,6 +84,17 @@ def merge_dfs(dfs: dict):
     df_conc = pd.concat(dfs, axis=1).sort_values(by='date')
     return df_conc
 
+def load_y(daterange = ['1973-01-01', '2017-01-01']):
+    df = pd.read_csv('../Andreini_data/data_transformed.csv', index_col=0)
+    df.index = pd.to_datetime(df.index)
+    #interpolate gpdc column
+    df['GDPC1'] = df['GDPC1'].interpolate(method='spline', order=1)
+    df = df.loc[daterange[0]:daterange[1]]
+    df = df.dropna(axis=1, how='any')
+    y = df.values
+    y= (y-y.min(axis=0))/(y.max(axis=0)-y.min(axis=0))
+    return y
+
 if __name__ == "__main__":
     codes = pd.read_csv('codes.txt', sep=' ')
     codes_fred = codes[codes['Source'] == 'FRED']
